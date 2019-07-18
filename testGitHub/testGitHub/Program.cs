f752b3bb98afaee5738ce893efcc69796b486622
Nikolaiskiqt <nikolaiskiqt@abv.bg>
2019-07-18 18:29:27 +0300

    First project

--- testGitHub/testGitHub/Program.cs
+++ testGitHub/testGitHub/Program.cs
@@ -6,7 +6,10 @@ namespace testGitHub
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hi");
             Console.WriteLine("Hello World!");
+            var a = 5;
+            Console.WriteLine(a);
         }
     }
 }
