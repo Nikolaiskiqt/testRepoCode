9a6bb94cc13c888171984bffacac6b7f2a4a749f
Nikolaiskiqt <nikolaiskiqt@abv.bg>
2019-07-18 18:27:26 +0300

    initial code added

---
+++ testGitHub/testGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
